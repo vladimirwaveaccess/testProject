842aa7237ead082086b9222fc26b3b853bd576df
Volodymyr Ahafonov <volodymyr.ahafonov@globallogic.com>
2019-06-11 19:03:02 +0300

    Some commit 4

---
+++ src/com/company/Static/Ball.java
@@ -0,0 +1,22 @@
+package com.company.Static;
+
+public class Ball {
+    public static int count = 0;
+
+    static int getCountInstance() {
+        return count;
+    }
+
+    private String color = "none";
+
+    public Ball(String color) {
+        this.color = color;
+        count++;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("ASD");
+        count--;
+    }
+}
